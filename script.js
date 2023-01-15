483265fca3e1e42e6b6ed670a37f97c245b6b2a4
Octet-Syntax <121711778+Octet-Syntax@users.noreply.github.com>
2023-01-15 20:36:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-fetch("https://blr1.blynk.cloud/external/api/isHardwareConnected?token=APuw3xTam8hTBHUTbSpt-c36Hf2HO2x0").then((data)=> {
-    return data.json();
-}).then((value)=> {
-    document.getElementById("textapi").innerHTML=value;
-})
+fetch("//your URL").then((data)=> { 
+    return data.json(); 
+}).then((value)=> { 
+    document.getElementById("textapi").innerHTML=value; 
+}) 
